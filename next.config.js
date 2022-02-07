d99e5746ff822bb381a57e10885f46e7dcc99759
sqrtthree <imsqrtthree@gmail.com>
2022-02-07 14:15:05 +0800

    chore: custom page extensions to ignore *.test.js files when in building

--- next.config.js
+++ next.config.js
@@ -6,4 +6,5 @@ const withNextra = nextra('nextra-theme-docs', './theme.config.js')
 
 module.exports = withNextra({
   assetPrefix: isProd ? '/leetcode/' : '',
+  pageExtensions: ['mdx', 'md', 'jsx', 'page.js'],
 })
